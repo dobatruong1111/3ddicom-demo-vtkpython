c9c3ffe836277731cc02e3bdf98d0561f1e6e994
dobatruong <batruongdo3@gmail.com>
2023-05-11 11:31:05 +0700

    updated lengthMeasurement, utils.py

--- measure/lengthMeasurement.py
+++ measure/lengthMeasurement.py
@@ -3,10 +3,10 @@ import utils
 from typing import List
 
 """
-    description:
-        class contains objects for drawing line in the world coordinate system
+    Description:
+        Class contains objects for drawing line in the world coordinate system.
 """
-class MeasureLengthPipeLine():
+class LengthMeasurementPipeline():
     def __init__(self) -> None:
         colors = vtk.vtkNamedColors()
         self.isDragging = False
@@ -43,7 +43,7 @@ class MeasureLengthPipeLine():
         self.lineActor.GetProperty().SetLineWidth(2)
         self.lineActor.VisibilityOff()
 
-        # display the length of two points in the world coordinate system
+        # Display the length of two points in the world coordinate system
         # vtkTextActor is an actor that displays text
         self.textActor = vtk.vtkTextActor()
         textProperty = self.textActor.GetTextProperty()
@@ -53,151 +53,167 @@ class MeasureLengthPipeLine():
         textProperty.BoldOn()
         self.textActor.VisibilityOff()
 
-        # marking the first point and the second point by two spheres
+        # Marking the first point and the second point by two spheres
         self.firstSphereActor = vtk.vtkActor()
-        self.firstSphereActor.GetProperty().SetColor(1, 0, 0)
+        self.firstSphereActor.GetProperty().SetColor(0, 1, 0)
         self.firstSphereActor.SetMapper(self.firstSphereMapper)
         self.firstSphereActor.VisibilityOff()
         
         self.secondSphereActor = vtk.vtkActor()
-        self.secondSphereActor.GetProperty().SetColor(1, 0, 0)
+        self.secondSphereActor.GetProperty().SetColor(0, 1, 0)
         self.secondSphereActor.SetMapper(self.secondSphereMapper)
         self.secondSphereActor.VisibilityOff()
 
 """
-    description: 
-        BeforeMeasureLengthInteractorStyle class extends vtkInteractorStyleTrackballCamera class
-        vtkInteractorStyleTrackballCamera allows the user to interactively manipulate (rotate, pan, etc.) the camera
-        Set interactor style before measure length 
+    Description: 
+        BeforeMeasureLengthInteractorStyle class extends vtkInteractorStyleTrackballCamera class.
+        vtkInteractorStyleTrackballCamera allows the user to interactively manipulate (rotate, pan, etc.) the camera.
+        Set interactor style before length measurement.
 """
-class BeforeMeasureLengthInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
-    def __init__(self, pipeline: MeasureLengthPipeLine) -> None:
+class BeforeLengthMeasurementInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, pipeline: LengthMeasurementPipeline) -> None:
         self.pipeline = pipeline
         self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, self.__leftButtonReleaseEvent)
 
     def __leftButtonReleaseEvent(self, obj: vtk.vtkInteractorStyleTrackballCamera, event: str) -> None:
         # Override method of super class
         self.OnLeftButtonUp()
-        
-        style = MeasureLengthInteractorStyle(self.pipeline)
+        style = LengthMeasurementInteractorStyle(self.pipeline)
         self.GetInteractor().SetInteractorStyle(style)
 
 """
-    description: 
-        MeasureLengthInteractorStyle class extends vtkInteractorStyleTrackballCamera class
-        Set interactor style for measure length
+    Description: 
+        MeasureLengthInteractorStyle class extends vtkInteractorStyleTrackballCamera class.
+        Set interactor style for length measurement.
 """
-class MeasureLengthInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
-    def __init__(self, pipeline: MeasureLengthPipeLine) -> None:
+class LengthMeasurementInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, pipeline: LengthMeasurementPipeline) -> None:
         self.pipeline = pipeline
+        self.checkNumberOfPoints = 0 # used to check current number of points, max = 2 points
         self.AddObserver(vtk.vtkCommand.LeftButtonPressEvent, self.__leftButtonPressEvent)
         self.AddObserver(vtk.vtkCommand.MouseMoveEvent, self.__mouseMoveEvent)
         self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, self.__leftButtonReleaseEvent)
 
+    """
+        Description:
+            A handle function when having mouse move event.
+            Used to mark the position of mouse in world coordinates when moving.
+            Used to draw a line connecting two points.
+    """
     def __mouseMoveEvent(self, obj: vtk.vtkInteractorStyleTrackballCamera, event: str) -> None:
         # vtkRenderer object
         renderer = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer()
-
         # vtkCamera object
         camera = renderer.GetActiveCamera()
-
         # The position of mouse in the display coordinate system
-        eventPosition = self.GetInteractor().GetEventPosition() # tuple
-
+        eventPosition = list(self.GetInteractor().GetEventPosition())
         # vtkCellPicker object, it shoots a ray into the volume and returns the point where the ray intersects an isosurface of a chosen opacity
         cellPicker = self.GetInteractor().GetPicker()
 
         if self.pipeline.isDragging:
-            # vtkPoints represents 3D points
-            points = self.pipeline.line.GetPoints()
-            firstPoint = points.GetPoint(0) # tuple
-            pickPosition = utils.getPickPosition(cellPicker, eventPosition, renderer, camera, True, firstPoint)
-            
-            # Marking the second point when drawing
-            self.pipeline.secondSphereActor.SetPosition(pickPosition)
-            self.pipeline.secondSphereActor.VisibilityOn()
+            if self.checkNumberOfPoints == 1:
+                # Return vtkPoints object
+                points = self.pipeline.line.GetPoints()
 
-            # Save the second point
-            points.SetPoint(1, pickPosition)
+                firstPoint = list(points.GetPoint(0)) # Return the first point
+                # Return a point in the world coordinate system on surface, if out then finding the projection point
+                pickPosition = utils.getPickPosition(eventPosition, cellPicker, renderer, camera, True, firstPoint)
+                
+                # Marking the second point when drawing
+                self.pipeline.secondSphereActor.SetPosition(pickPosition)
+                self.pipeline.secondSphereActor.VisibilityOn() # Turn on the second sphere
 
-            # vtkIdList may represent any type of integer id, but usually represents point and cell ids
-            idList = vtk.vtkIdList()
-            idList.InsertNextId(0) # Insert id of the first point
-            idList.InsertNextId(1) # Insert id of the second point
+                # Save the second point
+                points.SetPoint(1, pickPosition)
+                # Update the modification time for this object and its Data
+                points.Modified()
 
-            # Insert a cell of type VTK_LINE
-            self.pipeline.line.InsertNextCell(vtk.VTK_LINE, idList)
+                # vtkIdList may represent any type of integer id, but usually represents point and cell ids
+                idList = vtk.vtkIdList()
+                idList.InsertNextId(0) # Insert id of the first point
+                idList.InsertNextId(1) # Insert id of the second point
 
-            # Update the modification time for this object and its Data
-            points.Modified()
-
-            secondPoint = points.GetPoint(1) # tuple
-            # Calculate the middle point
-            midPoint = tuple(map(lambda i,j: (i+j)/2, firstPoint, secondPoint))
-            # Calculate the euclidean distance between the first point and the second point
-            distance = utils.getEuclideanDistanceBetween2Points(firstPoint, secondPoint)
-            # Convert the middle point from the world coordinate system to the display coordinate system 
-            displayCoords = utils.convertFromWorldCoords2DisplayCoords(renderer, midPoint)
-
-            # Display the distance and set position of text
-            self.pipeline.textActor.SetInput(f"{round(distance, 1)}mm")
-            self.pipeline.textActor.SetDisplayPosition(round(displayCoords[0]), round(displayCoords[1]))
-            self.pipeline.textActor.VisibilityOn()
+                # Insert a cell of type VTK_LINE
+                self.pipeline.line.InsertNextCell(vtk.VTK_LINE, idList)
+                # Method used to calculate the position of text actor
+                utils.buildTextActorLengthMeasurement(self.pipeline.textActor, renderer, points)
         else: # TODO: code need to processed in javascript
-            pickPosition = utils.getPickPosition(cellPicker, eventPosition, renderer, camera)
-            # Marking the position of mouse in the world coordinate system when moving mouse 
+            pickPosition = utils.getPickPosition(eventPosition, cellPicker, renderer, camera)
+            # Marking the position of mouse in world coordinates
             self.pipeline.firstSphereActor.SetPosition(pickPosition)
-            self.pipeline.firstSphereActor.VisibilityOn()
+            self.pipeline.firstSphereActor.VisibilityOn() # Turn on the first sphere
         self.GetInteractor().Render()
     
+    """
+        Description:
+            A handle function when having left button press event.
+            Used to mark the position of points in world coordinates when click.
+    """
     def __leftButtonPressEvent(self, obj: vtk.vtkInteractorStyleTrackballCamera, event: str) -> None:
-        self.pipeline.isDragging = True # Start drawing
+        # vtkRenderer object
         renderer = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer()
+        # vtkCamera object
         camera = renderer.GetActiveCamera()
-        eventPosition = self.GetInteractor().GetEventPosition() # the position of mouse in display coordinate system 
-        
+        # The position of mouse in the display coordinate system
+        eventPosition = list(self.GetInteractor().GetEventPosition()) # the position of mouse in display coordinate system 
         # vtkCellPicker object, it shoots a ray into the volume and returns the point where the ray intersects an isosurface of a chosen opacity
         cellPicker = self.GetInteractor().GetPicker()
 
-        pickPosition = utils.getPickPosition(cellPicker, eventPosition, renderer, camera)
+        self.checkNumberOfPoints += 1 # Add a point
+        if self.checkNumberOfPoints == 1:
+            self.pipeline.isDragging = True # Start drawing
+
+            # Return a point in the world coordinate system on surface or out
+            pickPosition = utils.getPickPosition(eventPosition, cellPicker, renderer, camera)
     
-        # Marking the first point when having left button down event
-        self.pipeline.firstSphereActor.SetPosition(pickPosition)
-        self.pipeline.firstSphereActor.VisibilityOn()
+            # Marking the first point when having left button down event
+            self.pipeline.firstSphereActor.GetProperty().SetColor(1, 0, 0)
+            self.pipeline.firstSphereActor.SetPosition(pickPosition)
 
-        # vtkPoints represents 3D points
-        points = vtk.vtkPoints()
-        # vtkCellArray object to represent cell connectivity
-        aline = vtk.vtkCellArray()
-        # Set objects into vtkPolyData object
-        self.pipeline.line.SetPoints(points)
-        self.pipeline.line.SetLines(aline)
+            # vtkPoints represents 3D points used to save 2 points in world coordinates
+            points = vtk.vtkPoints()
+            # vtkCellArray object to represent cell connectivity
+            aline = vtk.vtkCellArray()
+            # Set objects into vtkPolyData object
+            self.pipeline.line.SetPoints(points)
+            self.pipeline.line.SetLines(aline)
 
-        # Insert the first point into vtkPoints object when having left button down
-        points.InsertNextPoint(pickPosition)
-        # Insert the second point into vtkPoints object
-        points.InsertNextPoint(0, 0, 0) # defauld
+            # Insert the first point into vtkPoints object when having left button down
+            points.InsertNextPoint(pickPosition)
+            # Insert the second point into vtkPoints object
+            points.InsertNextPoint(0, 0, 0) # defauld
 
-        # Turn on line actor object
-        self.pipeline.lineActor.VisibilityOn()
+            self.pipeline.lineActor.VisibilityOn() # Turn on line actor object
+            self.pipeline.textActor.VisibilityOn() # Turn on text actor object
+        elif self.checkNumberOfPoints == 2:
+            points = self.pipeline.line.GetPoints()
+            pickPosition = points.GetPoint(1) # Return the second point
+            self.pipeline.secondSphereActor.GetProperty().SetColor(1, 0, 0) # Set red color for the second sphere
+            self.pipeline.secondSphereActor.SetPosition(pickPosition) # Set position for the second sphere
         # Override method of super class
         self.OnLeftButtonDown()
 
+    """
+        Description:
+            A handle function when having left button release event.
+            If number of points equals 2, set interactor style after length measurement finish.
+    """
     def __leftButtonReleaseEvent(self, obj: vtk.vtkInteractorStyleTrackballCamera, event: str) -> None:
-        self.pipeline.isDragging = False # Stop drawing
         # Override method of super class
         self.OnLeftButtonUp()
-        # Set interactor style when stop drawing
-        style = UpdateLengthPositionInteractorStyle(self.pipeline)
-        self.GetInteractor().SetInteractorStyle(style)
+        if self.checkNumberOfPoints == 2:
+            self.pipeline.isDragging = False # Stop drawing
+            # Set interactor style when stop drawing
+            style = AfterLengthMeasurementInteractorStyle(self.pipeline)
+            self.GetInteractor().SetInteractorStyle(style)
 
 """
-    description:
-        UpdateLengthPositionInteractorStyle class extends vtkInteractorStyleTrackballCamera class
-        Set interactor style after drawing will update the position of text when having mouse move event
+    Description:
+        UpdateLengthPositionInteractorStyle class extends vtkInteractorStyleTrackballCamera class.
+        Set interactor style after drawing will update the position of text when having mouse move event.
 """
-class UpdateLengthPositionInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
-    def __init__(self, pipeline: MeasureLengthPipeLine) -> None:
+class AfterLengthMeasurementInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, pipeline: LengthMeasurementPipeline) -> None:
         self.pipeline = pipeline
         self.AddObserver(vtk.vtkCommand.MouseMoveEvent, self.__mouseMoveEvent)
     
@@ -205,21 +221,18 @@ class UpdateLengthPositionInteractorStyle(vtk.vtkInteractorStyleTrackballCamera)
         renderer = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer()
         points = self.pipeline.line.GetPoints()
 
-        firstPoint = points.GetPoint(0); secondPoint = points.GetPoint(1)
-        midPoint = tuple(map(lambda i,j: (i+j)/2, firstPoint, secondPoint))
-        displayCoords = utils.convertFromWorldCoords2DisplayCoords(renderer, midPoint)
-        self.pipeline.textActor.SetDisplayPosition(round(displayCoords[0]), round(displayCoords[1]))
+        # Method used to update the position of text actor
+        utils.buildTextActorLengthMeasurement(self.pipeline.textActor, renderer, points)
 
         self.GetInteractor().Render()
         # Override method of super class
         self.OnMouseMove()
 
 """
-    description: 
-        calculate input data for transfer function
-    params:
+    Description: calculate input data for transfer function.
+    Params:
         colormap: a standard for color map with each CT number (HU)
-    return: a list contains other lists, sub lists have size = 4 with format: [CT number, red color, green color, blue color], colors between 0 and 1
+    Return: a list contains other lists, sub lists have size = 4 with format: [CT number, red color, green color, blue color], colors between 0 and 1
 """
 def to_rgb_points(colormap: List[dict]) -> List[list]:
     rgb_points = []
@@ -273,14 +286,13 @@ def main():
     volProperty = vtk.vtkVolumeProperty()
     render = vtk.vtkRenderer()
     renWin = vtk.vtkRenderWindow()
-    gradientOpacity = vtk.vtkPiecewiseFunction()
     scalarOpacity = vtk.vtkPiecewiseFunction()
     color = vtk.vtkColorTransferFunction()
     renIn = vtk.vtkRenderWindowInteractor()
-    pipeline = MeasureLengthPipeLine()
+    pipeline = LengthMeasurementPipeline()
 
-    # outline
-    # description: drawing a bounding box out volume object
+    # Outline
+    # Description: drawing a bounding box out volume object
     outline = vtk.vtkOutlineFilter()
     outline.SetInputConnection(reader.GetOutputPort())
     outlineMapper = vtk.vtkPolyDataMapper()
@@ -308,11 +320,11 @@ def main():
         color.AddRGBPoint(rgb_point[0], rgb_point[1], rgb_point[2], rgb_point[3])
     volProperty.SetColor(color)
 
-    # bone preset
+    # Bone preset
     scalarOpacity.AddPoint(80, 0)
     scalarOpacity.AddPoint(400, 0.2)
     scalarOpacity.AddPoint(1000, 1)
-    # muscle preset
+    # Muscle preset
     # scalarOpacity.AddPoint(-63.16470588235279, 0)
     # scalarOpacity.AddPoint(559.1764705882356, 1)
     volProperty.SetScalarOpacity(scalarOpacity)
@@ -338,7 +350,7 @@ def main():
     cellPicker.AddPickList(vol)
     cellPicker.PickFromListOn()
     renIn.SetPicker(cellPicker)
-    style = BeforeMeasureLengthInteractorStyle(pipeline)
+    style = BeforeLengthMeasurementInteractorStyle(pipeline)
     renIn.SetInteractorStyle(style)
 
     renIn.Initialize()
